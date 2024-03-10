22061abc5c9fac25d154ae4dea7addcda313a15b
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2024-03-10 15:31:38 +0900

    test4

---
+++ api/chat.ts
@@ -0,0 +1,8 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { name } = req.body;
+  return res.json({
+    message: `Chat ${name}!`,
+  });
+}
