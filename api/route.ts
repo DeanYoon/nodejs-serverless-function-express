38431c7bc0c5a4dc7a99e34977f340f93c3ae50a
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2024-03-10 15:24:50 +0900

    test2

--- api/route.ts
+++ api/route.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function GET(req: VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
+  const { name } = req.query;
   return res.json({
     message: `Hello ${name}!`,
   });
