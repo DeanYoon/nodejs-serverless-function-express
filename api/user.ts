8b7a747dbb8174054159ccc2ac7d38171a27348e
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2024-03-10 16:38:55 +0900

    re test

--- api/user.ts
+++ api/user.ts
@@ -3,16 +3,16 @@ import { User } from "../models/User";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { username } = req.body;
-
-  try {
-    const user = await User.findOne({ username: username });
-    if (user) {
-      res.status(201).send(user);
-    } else {
-      res.status(500).send("User not exists");
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong");
-  }
+  res.send(username);
+  //   try {
+  //     const user = await User.findOne({ username: username });
+  //     if (user) {
+  //       res.status(201).send(user);
+  //     } else {
+  //       res.status(500).send("User not exists");
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).send("Something went wrong");
+  //   }
 }
